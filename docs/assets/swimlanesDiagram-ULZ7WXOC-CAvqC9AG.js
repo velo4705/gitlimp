a37a61fb23733d5a4a56e93b400920f70bd5a1ca
velo4705 <velocity4293@gmail.com>
2026-08-17 09:14:52 +0530

    Revert "Fix syntax highlighting: rewrite hljs-theme.css with reliable dark mode overrides"
    This reverts commit 8c70edbe1e24f9c38a57a0a7d735290fb9d57dbf.

--- docs/assets/swimlanesDiagram-ULZ7WXOC-CAvqC9AG.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-UKHOOZJN-CX9ukJEw.js";import{_ as a}from"./index-AgQbjszm.js";import"./chunk-5VM5RSS4-DVH2l08X.js";import"./chunk-XXDRQBXY-D4GvNYeZ.js";import"./chunk-KBJHAD2P-BH42ZXZr.js";import"./chunk-2GRJ4B5K-CawYXJU4.js";import"./channel-Dloru7er.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
