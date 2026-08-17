8c70edbe1e24f9c38a57a0a7d735290fb9d57dbf
velo4705 <velocity4293@gmail.com>
2026-08-17 08:59:05 +0530

    Fix syntax highlighting: rewrite hljs-theme.css with reliable dark mode overrides

---
+++ docs/assets/swimlanesDiagram-ULZ7WXOC-BhAvb1OS.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-UKHOOZJN-Bfw1jIiJ.js";import{_ as a}from"./index-BMcvXac7.js";import"./chunk-5VM5RSS4-DR7zRWqd.js";import"./chunk-XXDRQBXY-CyPQV8kj.js";import"./chunk-KBJHAD2P-B_4kk2bG.js";import"./chunk-2GRJ4B5K-BDYeLz5z.js";import"./channel-CYnL61fs.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
