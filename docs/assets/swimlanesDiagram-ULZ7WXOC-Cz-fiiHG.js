db6cb7d4cf9f630ea20b402e878d8a8fb3297dc1
velo4705 <velocity4293@gmail.com>
2026-08-17 09:14:52 +0530

    Revert "Fix syntax highlighting: inline hljs styles to avoid @import CSS pipeline break"
    This reverts commit 9beb397e1d879fa0aa7825be24aeb0de84a84a43.

--- docs/assets/swimlanesDiagram-ULZ7WXOC-Cz-fiiHG.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-UKHOOZJN-DEMU9Eb0.js";import{_ as a}from"./index-GCZ8fTl5.js";import"./chunk-5VM5RSS4-CMVyn5kS.js";import"./chunk-XXDRQBXY-B9R7gbiU.js";import"./chunk-KBJHAD2P-DDCvscFr.js";import"./chunk-2GRJ4B5K-sc9BtTCO.js";import"./channel-BRsEvOsB.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
